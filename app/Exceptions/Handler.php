b743a3a8337d70c24c5c3e3c2014b5c0933e9b8a
Sumon Ahmed <bdsumon4u@gmail.com>
2022-09-01 18:30:27 +0600

    Exception Report On Slack Channel

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Log;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -35,7 +36,13 @@ class Handler extends ExceptionHandler
     public function register()
     {
         $this->reportable(function (Throwable $e) {
-            //
+            Log::channel('slack')->error($e->getMessage(), collect([
+                [
+                    'file' => $e->getFile(),
+                    'line' => $e->getLine(),
+                    'code' => $e->getCode(),
+                ],
+            ])->concat($e->getTrace())->take(6)->toArray());
         });
     }
 }
